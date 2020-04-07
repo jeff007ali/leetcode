87a47642a91db510116709a9f4e1c3e0734abac8
jeff007ali <jagharia@gmail.com>
2020-04-07 21:33:54 +0530

    fixed error of day 7 challenge of leetcode

--- 30-day-leetcoding-challenge/D07-CountingElements.py
+++ 30-day-leetcoding-challenge/D07-CountingElements.py
@@ -8,7 +8,7 @@ def countElements(arr):
     return ans
 
 
-print(countElements1([1,2,3]))
+print(countElements([1,2,3]))
 print(countElements([1,1,3,3,5,5,7,7]))
 print(countElements([1,3,2,3,5,0]))
 print(countElements([1,1,2,2]))
