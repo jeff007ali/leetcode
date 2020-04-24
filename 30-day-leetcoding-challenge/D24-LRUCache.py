8a912ac1d6bac15ceb517299c4041561654cf83f
jeff007ali <jagharia@gmail.com>
2020-04-24 23:07:31 +0530

    Day 24/30 days of leetcoding challenge

---
+++ 30-day-leetcoding-challenge/D24-LRUCache.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache[key]
+        self.cache.move_to_end(key)
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache: del self.cache[key]
+        self.cache[key] = value
+        if len(self.cache) > self.size:
+            self.cache.popitem(last=False)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
