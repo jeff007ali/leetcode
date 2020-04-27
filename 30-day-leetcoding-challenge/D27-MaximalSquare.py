8d456d88a048f28743129a3ff2f7c740f10c47ab
jeff007ali <jagharia@gmail.com>
2020-04-27 22:23:48 +0530

    Syntax error

--- 30-day-leetcoding-challenge/D27-MaximalSquare.py
+++ 30-day-leetcoding-challenge/D27-MaximalSquare.py
@@ -6,7 +6,7 @@ def maximalSquare(matrix):
     n = len(matrix[0])
     
     dp = [[0] * (n+1) for _ in range(m+1)]
-    ans = 0;
+    ans = 0
     
     for i in range(m):
         for j in range(n):
