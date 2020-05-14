1398866464d7e8d565e5fbff2a4b8a36d4fd2ddc
jeff007ali <jagharia@gmail.com>
2020-05-14 22:42:25 +0530

    Day 14 of May leetcoding challenge

---
+++ may-leetcoding-challenge/D14-ImplementTriePrefixTree.py
@@ -0,0 +1,43 @@
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.memory = []
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        if word not in self.memory:
+            self.memory.append(word)
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        if word in self.memory:
+            return True
+        else:
+            return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        for word in self.memory:
+            if word.startswith(prefix):
+                return True
+        
+        return False
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
