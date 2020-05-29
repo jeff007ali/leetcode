26a96c5574adc2d5d693a30de855b96dfcfa2689
jeff007ali <jagharia@gmail.com>
2020-05-29 18:56:27 +0530

    Day 29 of May leetcoding challenge

---
+++ may-leetcoding-challenge/D29-CourseSchedule.py
@@ -0,0 +1,38 @@
+# Using topological sort
+# 1. Calculate incoming arrow to every node(inDegree - list)
+# 2. If any node is having ZERO incoming arrow, then add that node to stack
+# 3. Iterate over stack until it's empty
+# 4. In loop, pop last element from stack and remove every connection from that node and update inDegree(list)
+# 5. After update, check if any node is having ZERO incoming arrow, then again add that node to stack
+# 6. After every element pop, increase COUNT by one
+# 7. Compare COUNT and num of courses
+
+def canFinish(numCourses, prerequisites):
+    inDegree = [0] * numCourses
+    count = 0
+
+    for i in range(len(prerequisites)):
+        inDegree[prerequisites[i][0]] += 1
+
+    stack = []
+
+    for i in range(len(inDegree)):
+        if inDegree[i] == 0:
+            stack.append(i)
+    
+    while stack:
+        learned = stack.pop()
+        count += 1
+
+        for i in range(len(prerequisites)):
+            if prerequisites[i][1] == learned:
+                inDegree[prerequisites[i][0]] -= 1
+
+                if inDegree[prerequisites[i][0]] == 0:
+                    stack.append(prerequisites[i][0])
+    
+    return count == numCourses
+
+
+print(canFinish(2, [[1,0]]))
+print(canFinish(2, [[1,0],[0,1]]))
