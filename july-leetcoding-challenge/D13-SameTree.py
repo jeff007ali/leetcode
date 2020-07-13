509c1415c54cf0ffd9a6a45060b65433ab3a3930
jeff007ali <jagharia@gmail.com>
2020-07-13 23:44:34 +0530

    Day 13 of July leetcoding challenge

---
+++ july-leetcoding-challenge/D13-SameTree.py
@@ -0,0 +1,34 @@
+# Method 1 - Recursion 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+
+
+
+
+# Method 2 - Tuples
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        def t(n):
+            return n and (n.val, t(n.left), t(n.right))
+        
+        return t(p) == t(q)
+
+
+
