383506e67273a6df502b8f04a154ae22859c7c2c
jeff007ali <jagharia@gmail.com>
2020-10-10 20:03:12 +0530

    Completed day 10 of October leetcoding challenge

---
+++ october-leetcoding-challenge/D10-MinimumNumberOfArrowsToBurstBalloons.py
@@ -0,0 +1,15 @@
+def findMinArrowShots(points):
+    points = sorted(points, key=lambda p: p[1])
+    res = 0
+    end = -float('inf')
+
+    for interval in points:
+        if interval[0] > end:
+            res += 1
+            end = interval[1]
+
+    return res
+
+
+print(findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]))
+print(findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]))
